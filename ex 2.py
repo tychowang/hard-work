4f6fa488a516f3ebf33ad6f43bf35ad823788913
tychowang <56498601+tychowang@users.noreply.github.com>
2019-10-13 20:16:55 +0800

    Create ex 2.py

---
+++ ex 2.py
@@ -0,0 +1,23 @@
+print ("I will now count my chickens:")
+
+print ("hens",25+30/6)
+print ("roosters",100-25*3%4)
+
+print ("now I will count the egg :")
+
+print (3+2+1-5+4%2-1/4+6)
+
+print ("It is ture that 3+2<5-7?")
+
+print (3+2<5-7)
+
+print ("what is 3+2?",3+2)
+print ("what is 5-7?",5-7)
+
+print ("oh,that's why it's false.")
+
+print ("how about some more.")
+
+print ("Is it greater?",5>-2)
+print ("Is it greater or equal?",5>=-2)
+print ("Is it less or equal?",5<=-2)
