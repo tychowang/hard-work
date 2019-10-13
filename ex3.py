b4daac16b01816aa54a6fba417b64effa77f072e
tychowang <56498601+tychowang@users.noreply.github.com>
2019-10-13 20:33:57 +0800

    Update ex3.py
    %是取余数的意思

--- ex3.py
+++ ex3.py
@@ -1,13 +1,13 @@
 print ("I will now count my chickens:")
 
-print ("hens",25+30/6)
-print ("roosters",100-25*3%4)
+print ("hens",25+30/6) # /是除
+print ("roosters",100-25*3%4) #？75%4余数为3
 
 print ("now I will count the egg :")
 
-print (3+2+1-5+4%2-1/4+6)
+print (3+2+1-5+4%2-1/4+6) #4%2余数为0
 
-print ("It is ture that 3+2<5-7?")
+print ("It is ture that 3+2<5-7?") #bool值
 
 print (3+2<5-7)
 
