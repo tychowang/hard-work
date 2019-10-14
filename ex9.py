41426d7c58d6de0374b0e3443278888c36d3205c
tychowang <56498601+tychowang@users.noreply.github.com>
2019-10-14 14:52:53 +0800

    n\,换行

---
+++ ex9.py
@@ -0,0 +1,13 @@
+#ex9.py  printing printing printing
+
+days = "Mon Tue Wed THu Fri Sat Sun"
+months = "Jan\nFeb\nMAr\nApr\nMAy\nJun\nJul\nAug" #n\代表换行
+
+print("Here are the days: ",days)
+print("here are the months: ",months)
+
+print("""There's something going on here.
+With the three double-quotes.
+we'll be able to type as much as we like.
+Even 4 lines if we want ,or 5,or6.
+""")
