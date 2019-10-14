3b8906903230773d1e347d11939e240437c9d811
tychowang <56498601+tychowang@users.noreply.github.com>
2019-10-14 13:54:43 +0800

    ex7.py

---
+++ ex7.py
@@ -0,0 +1,21 @@
+#ex7.py more painting
+print("mary had a little lamb.")
+print("its fleece was white as {}.".format('snow')) #{}空格处加0结果不变，0表示format（）中的第一个数。
+print("And everywhere that mary went.")
+print("." * 10) #省略号....(10个)
+
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print(end1 + end2 + end3 + end4 + end5 + end6 ,end=' ') #end' '中间的空格来自于此
+print(end7 + end8 + end9 + end10 + end11 + end12) 
