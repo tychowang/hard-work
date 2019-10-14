fe098e3f73442c6e7f80ade07caba1af1d0071f4
tychowang <56498601+tychowang@users.noreply.github.com>
2019-10-14 16:01:34 +0800

    ex12.py

---
+++ ex12.py
@@ -0,0 +1,7 @@
+#ex12.py y = input ("name?") :name?_下划线中可填入名字
+age = input ("How old are you? ") #输入的内容指向了age
+height = input ("How tall are you? ")
+weight = input ("How much do you weight? ")
+
+print(f"so,you are {age}old,{height}tall and {weight}heavy")
+
