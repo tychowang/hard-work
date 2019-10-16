1733f3070622efc11c79f92bf0f5bed883096bd7
tychowang <56498601+tychowang@users.noreply.github.com>
2019-10-16 09:57:24 +0800

    ex13.2.py
    script,first,second,third = argv,argv,argv,argv

---
+++ ex13.2.py
@@ -0,0 +1,9 @@
+#ex13.py parameter,unpacking,variables(参数，拆包，变量)
+
+from sys import argv
+script,first,second,third = argv,argv,argv,argv
+
+print("The script is called:",script)
+print("Your first variable is:",first)
+print("Your second variable is:",second)
+print("Your third variable is:",third)
